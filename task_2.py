4b9a342b07257fa8ce1c368fd7682aa75643abdf
Ahmad Alhineidi <ahmadhineidi@hotmail.com>
2023-06-04 12:51:25 +0200

    initial commit

---
+++ task_2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Let's suppose we have the following DataFrame
+df = pd.DataFrame({
+    'Name': ['John', 'Anna', 'Peter', 'Linda'],
+    'Age': [23, 78, 22, 19],
+    'Country': ['USA', 'USA', 'Canada', 'Australia']
+})
+
+# Print the entire DataFrame
+print(df)
+
+# Task 1: Implement a function to calculate and print the average age of people in the DataFrame.
+# Task 2: Implement a function that counts and prints the number of unique countries represented in the DataFrame.
+
