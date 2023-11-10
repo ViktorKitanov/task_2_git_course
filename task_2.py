2fe4fd7297a5cf9760e703c9df925bf8838192ae
ViktorKitanov <viktor.kitanov@hotmail.ch>
2023-11-10 12:33:24 +0100

    Completed tasks

--- task_2.py
+++ task_2.py
@@ -11,5 +11,19 @@ df = pd.DataFrame({
 print(df)
 
 # Task 1: Implement a function to calculate and print the average age of people in the DataFrame.
+def calculate_average_age(data_frame):
+    average_age = data_frame['Age'].mean()
+    print(f"Average Age: {average_age:.2f}")
+
 # Task 2: Implement a function that counts and prints the number of unique countries represented in the DataFrame.
+def count_unique_countries(data_frame):
+    unique_countries = data_frame['Country'].nunique()
+    print(f"Number of Unique Countries: {unique_countries}")
+
+# Task 1
+calculate_average_age(df)
+
+# Task 2
+count_unique_countries(df)
+
 
